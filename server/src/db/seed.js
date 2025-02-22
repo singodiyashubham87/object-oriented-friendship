6872f08a11fd5d3bf0f3bfcd666ffc1a2d0e1f31
Shubham Singodiya <singodiyashubham87@gmail.com>
2025-02-23 04:00:06 +0530

    configure db seeding

---
+++ server/src/db/seed.js
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import { seed } from "drizzle-seed";
+import { Bookmarks, Chats, Messages, Requests, Users } from "./schema.js";
+
+const seedCount = 10;
+
+async function seedDatabase(seedCount) {
+  const sql = neon(process.env.DATABASE_URL);
+  const db = drizzle(sql);
+  try {
+    console.log("🌱 Starting database seeding...🌱");
+    await seed(
+      db,
+      { Users, Requests, Bookmarks, Chats, Messages },
+      { count: seedCount },
+    );
+    console.log("✅ Database seeding completed successfully.");
+  } catch (error) {
+    console.error("❌ Error during database seeding:", error);
+  }
+}
+
+seedDatabase(seedCount);

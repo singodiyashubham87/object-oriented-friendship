929e2bb29282b7d209bcb136ca6c42161aa3c4bf
Shubham Singodiya <singodiyashubham87@gmail.com>
2025-03-06 00:16:27 +0530

    chore: add dummy signup router, validator, service & controller for user entity

--- server/src/modules/user/user.router.js
+++ server/src/modules/user/user.router.js
@@ -2,7 +2,9 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "Hello from user module!" });
+	res.json({ message: "Hello from user module!" });
 });
+
+router.post("/signup", userController.signup);
 
 export { router };

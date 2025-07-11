46057f3a890d222e1351f0a2cb200ca0d98f4462
Shubham Singodiya <singodiyashubham87@gmail.com>
2025-07-12 03:43:57 +0530

    feat: integrate feed api

--- client/src/pages/Feed/Feed.jsx
+++ client/src/pages/Feed/Feed.jsx
@@ -1,16 +1,23 @@
+import Loader from "@/components/Loader";
 import AcceptRequestIcon from "@/components/icons/AcceptRequestIcon";
 import BookmarkRequestUserIcon from "@/components/icons/BookmarkRequestUserIcon";
 import FeedNextArrowIcon from "@/components/icons/FeedNextArrowIcon";
 import FeedPrevArrowIcon from "@/components/icons/FeedPrevArrowIcon";
 import LocationIcon from "@/components/icons/LocationIcon";
 import RejectRequestIcon from "@/components/icons/RejectRequestIcon";
-import { useState } from "react";
+import axiosInstance from "@/config/axios";
+import { get, size } from "lodash-es";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import userAvatar from "../../assets/images/userAvatar.png";
 import { feedData, indianStatesMap } from "./feedData";
 
 const Feed = () => {
   const navigate = useNavigate();
   const [currentIndex, setCurrentIndex] = useState(1);
+  const [feedUsers, setFeedUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [prevArrowColor, setPrevArrowColor] = useState("#C7C2C2");
   const [nextArrowColor, setNextArrowColor] = useState("#C7C2C2");
   const transitionStyle = "ease-in-out transition-transform duration-300";
@@ -51,6 +58,43 @@ const Feed = () => {
     alert("User bookmarked!");
   };
 
+  useEffect(() => {
+    const fetchFeedData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await axiosInstance.get("/user/feed");
+        const feedUsers = get(res, "data.data.feed", []);
+
+        if (!size(feedUsers)) {
+          toast.error("No users found in the feed.");
+          return;
+        }
+
+        setFeedUsers(feedUsers);
+      } catch (error) {
+        toast.error("Failed to fetch feed data.");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchFeedData();
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (!size(feedUsers)) {
+    return (
+      <div className="flex items-center justify-center h-full">
+        <p className="text-primary-silver text-2xl">
+          No users found in the feed.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex-grow flex flex-col justify-evenly items-center w-full h-11/12 bg-dark-glassmorphism-30 border-xs border-secondary-silver rounded-custom-s overflow-y-auto overflow-x-hidden px-6 py-6">
       <div className="flex justify-center h-1/5 ">
@@ -82,17 +126,14 @@ const Feed = () => {
             >
               <div className="h-[10rem] border-2 border-primary-dark rounded-custom-xs object-cover overflow-hidden">
                 <img
-                  src={
-                    feedData[currentIndex - 1]?.image ||
-                    "https://media.istockphoto.com/id/1408596915/photo/employees-must-complete-the-online-survey-form-answer-the-test-questions-marked-checklist-on.jpg?s=2048x2048&w=is&k=20&c=JCkoENdhBCbi0DC0lIi1did1rHfNd8wu-AVq2bduFyw="
-                  }
+                  src={feedUsers[currentIndex - 1]?.avatar || userAvatar}
                   alt="Friend"
                   className="w-56 h-56 rounded-xl"
                 />
               </div>
               <div className="flex justify-between">
                 <p className="text-center text-primary-dark font-bold text-lg">
-                  {feedData[currentIndex]?.name}
+                  {feedUsers[currentIndex - 1]?.firstName}
                 </p>
                 <div className="flex items-center gap-1">
                   <LocationIcon
@@ -101,15 +142,17 @@ const Feed = () => {
                     styles={{ paddingBottom: "0.2rem" }}
                   />
                   <p className="text-primary-dark">
-                    {indianStatesMap[feedData[currentIndex]?.state]}
+                    {indianStatesMap[feedUsers[currentIndex - 1]?.location]}
                   </p>
                 </div>
               </div>
               <div className="w-1/4 flex bg-primary-gray rounded-custom-xxs text-primary-dark border border-primary-dark font-semibold justify-center items-center gap-2">
                 <p className="text-primary-dark">{`${
-                  feedData[currentIndex]?.age || 21
+                  feedUsers[currentIndex - 1]?.age || 21
                 }, `}</p>
-                <p className="text-primary-dark">{`${feedData[currentIndex]?.gender}`}</p>
+                <p className="text-primary-dark">{`${
+                  feedUsers[currentIndex - 1]?.gender
+                }`}</p>
               </div>
               <div className="buttons flex gap-8 mt-4 justify-center">
                 <div className="p-2 bg-primary-pink hover:bg-primary-pink-70 rounded-full border-xs border-primary-dark cursor-pointer">
@@ -126,7 +169,6 @@ const Feed = () => {
           )}
 
           {/* Active card */}
-
           <div
             className={`hover:scale-[1.02] ${transitionStyle} z-10 cursor-pointer min-w-[19rem] min-h-[23rem] flex flex-col gap-2 border border-gray-400 rounded-custom-s bg-primary-silver shadow-lg p-6`}
             onClick={(e) => handleProfileClick(e)}
@@ -134,17 +176,14 @@ const Feed = () => {
           >
             <div className="w-full h-[13rem] border-2 border-primary-dark rounded-custom-xs object-cover overflow-hidden">
               <img
-                src={
-                  feedData[currentIndex]?.image ||
-                  "https://imgs.search.brave.com/sMcgal2XI2iAo_tND_9PgnZWVnna0tvZoWgZnUnuQDw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NDFtMTcteWR6TEwu/anBn"
-                }
+                src={feedUsers[currentIndex]?.avatar || userAvatar}
                 alt="Friend"
                 className="w-full h-full"
               />
             </div>
             <div className="flex justify-between">
               <p className="text-center text-primary-dark font-bold text-lg">
-                {feedData[currentIndex]?.name}
+                {feedUsers[currentIndex]?.firstName}
               </p>
               <div className="flex items-center gap-1">
                 <LocationIcon
@@ -153,35 +192,35 @@ const Feed = () => {
                   styles={{ paddingBottom: "0.2rem" }}
                 />
                 <p className="text-primary-dark">
-                  {indianStatesMap[feedData[currentIndex]?.state]}
+                  {indianStatesMap[feedUsers[currentIndex]?.location]}
                 </p>
               </div>
             </div>
             <div className="w-1/4 flex bg-primary-gray rounded-custom-xxs text-primary-dark border border-primary-dark font-semibold justify-center items-center gap-2">
               <p className="text-primary-dark">{`${
-                feedData[currentIndex]?.age || 21
+                feedUsers[currentIndex]?.age || 21
               }, `}</p>
-              <p className="text-primary-dark">{`${feedData[currentIndex]?.gender}`}</p>
+              <p className="text-primary-dark">{`${feedUsers[currentIndex]?.gender}`}</p>
             </div>
             <div className="buttons flex gap-8 mt-4 justify-center">
               <button
                 type="button"
                 className="p-2 bg-primary-pink hover:bg-primary-pink-70 rounded-full border-xs border-primary-dark cursor-pointer"
-                onClick={() => handleRejectClick(feedData[currentIndex]?.id)}
+                onClick={() => handleRejectClick(feedUsers[currentIndex]?.id)}
               >
                 <RejectRequestIcon size="26" />
               </button>
               <button
                 type="button"
                 className="p-2 bg-primary-cyan hover:bg-primary-cyan-70 rounded-full border-xs border-primary-dark cursor-pointer"
-                onClick={() => handleBookmarkClick(feedData[currentIndex]?.id)}
+                onClick={() => handleBookmarkClick(feedUsers[currentIndex]?.id)}
               >
                 <BookmarkRequestUserIcon size="26" />
               </button>
               <button
                 type="button"
                 className="p-2 bg-primary-green hover:bg-primary-green-70 rounded-full border-xs border-primary-dark cursor-pointer"
-                onClick={() => handleAcceptClick(feedData[currentIndex]?.id)}
+                onClick={() => handleAcceptClick(feedUsers[currentIndex]?.id)}
               >
                 <AcceptRequestIcon size="26" />
               </button>
@@ -189,7 +228,7 @@ const Feed = () => {
           </div>
 
           {/* Right faded card */}
-          {currentIndex < feedData.length - 1 && (
+          {currentIndex < feedUsers.length - 1 && (
             // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
             <div
               className={`absolute hover:scale-[0.95] ${transitionStyle} cursor-pointer flex flex-col gap-2 right-16 min-w-[15rem] min-h-[19rem] border border-gray-300 bg-primary-silver opacity-50 scale-90 rounded-custom-s transition-transform p-6`}
@@ -197,17 +236,14 @@ const Feed = () => {
             >
               <div className="h-[10rem] border-2 border-primary-dark rounded-custom-xs object-cover overflow-hidden">
                 <img
-                  src={
-                    feedData[currentIndex + 1]?.image ||
-                    "https://media.istockphoto.com/id/1434437996/photo/woman-hand-writing-on-clipboard-with-a-pen.jpg?s=2048x2048&w=is&k=20&c=r80A-T41z3wwxA-3LD_nYLF3rxMl0JrmS--dULY9TcY="
-                  }
+                  src={feedUsers[currentIndex + 1]?.avatar || userAvatar}
                   alt="Friend"
                   className="w-56 h-56 rounded-xl"
                 />
               </div>
               <div className="flex justify-between">
                 <p className="text-center text-primary-dark font-bold text-lg">
-                  {feedData[currentIndex]?.name}
+                  {feedUsers[currentIndex + 1]?.firstName}
                 </p>
                 <div className="flex items-center gap-1">
                   <LocationIcon
@@ -216,15 +252,17 @@ const Feed = () => {
                     styles={{ paddingBottom: "0.2rem" }}
                   />
                   <p className="text-primary-dark">
-                    {indianStatesMap[feedData[currentIndex]?.state]}
+                    {indianStatesMap[feedUsers[currentIndex + 1]?.location]}
                   </p>
                 </div>
               </div>
               <div className="w-1/4 flex bg-primary-gray rounded-custom-xxs text-primary-dark border border-primary-dark font-semibold justify-center items-center gap-2">
                 <p className="text-primary-dark">{`${
-                  feedData[currentIndex]?.age || 21
+                  feedUsers[currentIndex + 1]?.age || 21
                 }, `}</p>
-                <p className="text-primary-dark">{`${feedData[currentIndex]?.gender}`}</p>
+                <p className="text-primary-dark">{`${
+                  feedUsers[currentIndex + 1]?.gender
+                }`}</p>
               </div>
               <div className="buttons flex gap-8 mt-4 justify-center">
                 <div className="p-2 bg-primary-pink hover:bg-primary-pink-70 rounded-full border-xs border-primary-dark cursor-pointer">
@@ -245,11 +283,11 @@ const Feed = () => {
           onClick={handleNext}
           type="button"
           className={` ${
-            currentIndex === feedData.length - 1
+            currentIndex === feedUsers.length - 1
               ? "opacity-50 cursor-not-allowed"
               : ""
           } hover:scale-[1.1] ${transitionStyle}`}
-          disabled={currentIndex === feedData.length - 1}
+          disabled={currentIndex === feedUsers.length - 1}
           onMouseEnter={() => setNextArrowColor("#e7e7e7")}
           onMouseLeave={() => setNextArrowColor("#c7c2c2")}
         >

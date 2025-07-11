cebb75b2ab3c7f13ef1dd4bce6c0b3b67f3c57a4
Shubham Singodiya <singodiyashubham87@gmail.com>
2025-07-12 00:13:52 +0530

    feat: add migrations for user schema update

---
+++ server/src/db/migrations/0005_aberrant_satana.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "user" ADD COLUMN "location" text;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "age" integer;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "gender" text;

c0b102ba814b09cd844f9fac48476cca37775879
singodiyashubham87 <singodiyashubham87@gmail.com>
2025-12-19 22:58:30 +0530

    style: update loader z-index

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/images/oof-web-logo.png";
 
 const Loader = () => {
   return (
-    <div className="loaderContainer w-[100vw] fixed top-[0] bottom-[0] right-[0] left-[0] bg-[#272829] flex justify-center items-center opacity-[90%] z-[2]">
+    <div className="loaderContainer w-[100vw] fixed top-[0] bottom-[0] right-[0] left-[0] bg-[#272829] flex justify-center items-center opacity-[90%] z-[1000]">
       <img
         id="logo"
         src={logo}
